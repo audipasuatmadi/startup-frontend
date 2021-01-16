037629428a8d28f1a31efa3eebfe21a6d9415aa0
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-01-16 19:24:53 +0800

    integrated tailwindcss and material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles'
+import { yellow } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000'
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
