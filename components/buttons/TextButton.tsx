402107c7bb18fef37a628438635f5292fea6f9f1
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-01-17 20:35:18 +0800

    Migrated TextButton with the tests

---
+++ components/buttons/TextButton.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import BaseButton from './BaseButton';
+
+export interface TextButtonProps {
+  disabled?: boolean;
+  href?: string;
+  children?: any;
+
+  className?: string;
+  type?: 'button' | 'submit' | 'reset';
+
+  onClick?: () => void;
+  theme?: 'brand' | 'gray';
+}
+
+const TextButton: React.FC<TextButtonProps> = ({
+  className,
+  theme,
+  disabled,
+  ...otherProps
+}: TextButtonProps) => {
+  const themeList: [string, string][] = [];
+  themeList['brand'] = ['brand', 'brand-light'];
+  themeList['gray'] = ['gray-500', 'gray-300'];
+
+  const [bgColor, bgLighterColor] = themeList[theme];
+
+  const disabledFalseTheme = `text-${bgColor} transition-colors duration-200 hover:bg-opacity-30 hover:bg-${bgLighterColor} active:bg-${bgColor} focus:ring-4 ring-${bgColor} ring-opacity-50`;
+  const disabledTrueTheme = `text-gray-400 cursor-default`;
+
+  const finalStyles = `button bg-none ${
+    disabled ? disabledTrueTheme : disabledFalseTheme
+  } `;
+
+  return (
+    <BaseButton
+      className={`${finalStyles}${className ? ` ${className}` : ''}`}
+      disabled={disabled}
+      {...otherProps}
+    />
+  );
+};
+
+TextButton.defaultProps = {
+  theme: 'brand',
+};
+
+export default TextButton;
