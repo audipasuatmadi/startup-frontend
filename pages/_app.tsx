29b31aac59acf215ca7ce53c7b8f8c95ab84bca8
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-09 15:21:20 +0800

    creates FinalNavbar and installed draft.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,19 @@ import { Provider } from 'react-redux'
 import { useStore } from '../states/Store'
 import Navbar from '../components/navbars/Navbar'
 import { Store } from 'redux'
+import { useRouter } from 'next/router'
+
+const FinalNavbar = ({path}: {path: string}) => (
+  path != '/articles/new' && <Navbar />
+)
 
 function MyApp({ Component, pageProps }) {
   const store: Store = useStore(pageProps.initialReduxState)
-  
+  const router = useRouter();
 
   return (
     <Provider store={store}>
-      <Navbar />
+      <FinalNavbar path={router.pathname} />
       <Component {...pageProps} />
     </Provider>
   )
---
+++ pages/articles/new.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const newArticle = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default newArticle
