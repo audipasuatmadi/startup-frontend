d6ae13fd1c54ca1a0c152211397a735825a328ab
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-11 18:12:42 +0800

    implement minimum stateless desktop frontend

--- components/navbars/DevNavbar.tsx
+++ components/navbars/DevNavbar.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import Image from 'next/image';
 import IconButton from '../buttons/IconButton';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../states/RootReducer';
+import { widthHigherThan } from '../../lib/utils/WindowUtil';
 
 interface Props {}
 
-const DevNavbar = (props: Props) => {
-  return (
-    <nav className='fixed w-screen flex items-center justify-between py-2 shadow h-14 bg-brand z-50 top-0'>
+const MobileDevMenu = () => (
+  <>
+    <IconButton
+      icon={
+        <Image
+          src='/assets/svgs/ic-back-button.svg'
+          width={25}
+          height={25}
+          alt='search bar image'
+        />
+      }
+      className='ml-3'
+    />
+
+    <h1 className='text-white'>Unnamed Article</h1>
+
+    <IconButton
+      icon={
+        <Image
+          src='/assets/svgs/ic-gear.svg'
+          width={25}
+          height={25}
+          alt='search bar image'
+        />
+      }
+      className='mr-3'
+    />
+  </>
+);
+
+const DesktopDevMenu = () => (
+  <>
+    <div className="flex gap-10">
       <IconButton
         icon={
           <Image
@@ -16,22 +49,82 @@ const DevNavbar = (props: Props) => {
             alt='search bar image'
           />
         }
-        className="ml-3"
+        className='ml-3'
       />
-
-      <h1 className="text-white">Unnamed Article</h1>
-
       <IconButton
         icon={
           <Image
-            src='/assets/svgs/ic-gear.svg'
+            src='/assets/svgs/ic-save.svg'
             width={25}
             height={25}
             alt='search bar image'
           />
         }
-        className="mr-3"
+        className='ml-3'
       />
+      <IconButton
+        icon={
+          <Image
+            src='/assets/svgs/ic-upload.svg'
+            width={25}
+            height={25}
+            alt='search bar image'
+          />
+        }
+        className='ml-3'
+      />
+    </div>
+
+    <h1 className='text-white'>Unnamed Article</h1>
+
+    <div className='flex gap-10'>
+      <IconButton
+        icon={
+          <Image
+            src='/assets/svgs/ic-barchart.svg'
+            width={25}
+            height={25}
+            alt='search bar image'
+          />
+        }
+        className='mr-3'
+      />
+      <IconButton
+        icon={
+          <Image
+            src='/assets/svgs/ic-play.svg'
+            width={25}
+            height={25}
+            alt='search bar image'
+          />
+        }
+        className='mr-3'
+      />
+      <IconButton
+        icon={
+          <Image
+            src='/assets/svgs/ic-question.svg'
+            width={25}
+            height={25}
+            alt='search bar image'
+          />
+        }
+        className='mr-3'
+      />
+    </div>
+  </>
+);
+
+const DevNavbar = (props: Props) => {
+  const screenSize = useSelector((state: RootState) => state.windowWidth);
+
+  return (
+    <nav className='fixed w-screen flex items-center justify-between py-2 shadow h-14 bg-brand z-50 top-0'>
+      {
+        widthHigherThan(screenSize, 'sm')
+          ? <DesktopDevMenu />
+          : <MobileDevMenu />
+      }
     </nav>
   );
 };
