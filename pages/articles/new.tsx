257fa57984e21cfb5644995a8af04fad6e18e9ff
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-20 19:37:59 +0800

    Add/Dispatch in New Articles Page

--- pages/articles/new.tsx
+++ pages/articles/new.tsx
@@ -4,7 +4,8 @@ import WriteToolbar from '../../components/navbars/WriteToolbar';
 import ArticleField from '../../components/inputfields/ArticleField';
 import { EditorState, RichUtils, convertToRaw } from 'draft-js';
 import createImagePlugin from '@draft-js-plugins/image';
-import Modal from '../../components/modals/Modal';
+import { useDispatch } from 'react-redux'
+import ArticleThunks from '../../lib/article/ArticleActions';
 
 const imagePlugin = createImagePlugin();
 const plugins = [imagePlugin];
@@ -13,6 +14,7 @@ const newArticle = () => {
   const [articleState, setArticleState] = useState<EditorState>(
     EditorState.createEmpty()
   );
+  const dispatch = useDispatch();
   const handleRichText = (inlineStyle: string) => (
     e: React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -39,6 +41,7 @@ const newArticle = () => {
     e.preventDefault();
     const contentState = articleState.getCurrentContent();
     const preparedContent = JSON.stringify(convertToRaw(contentState));
+    dispatch(ArticleThunks.saveArticle(preparedContent));
   }
 
   return (
