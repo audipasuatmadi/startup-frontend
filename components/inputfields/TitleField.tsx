4d20c34992b11f45b4414da640565498456aeb16
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-22 19:43:58 +0800

    Fix/Styling, Add/MaxLength to TextArea

--- components/inputfields/TitleField.tsx
+++ components/inputfields/TitleField.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 
-interface TitleField extends React.HTMLProps<HTMLInputElement> {}
-
+interface TitleField extends React.HTMLProps<HTMLTextAreaElement> {}
 const TitleField = ({ id, key, className, placeholder, ...otherProps }: TitleField) => {
   return (
     <form 
-      className="w-1/2"
+      className="w-3/4"
       autoComplete="off"
     >
-      <input
+      <textarea
         className={
           `focus-within:border-brand focus-within:ring-4 
           ring-brand ring-opacity-50 focus:outline-none 
-          rounded-sm w-full p-2 font-bold text-center text-5xl text-brand-light`
+          rounded-sm w-full p-2 font-bold text-center text-5xl text-brand-light
+          whitespace-pre-line`
         }
+        rows={1}
+        maxLength={26}
         placeholder="Judul"
+        style={{
+          resize: 'none'
+        }}
         {...otherProps}
       />
     </form>
