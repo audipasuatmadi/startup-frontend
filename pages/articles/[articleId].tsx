61f54185b7cba43ad09d63a9310c5e08df8ffe90
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-24 15:36:24 +0800

    Add/View Layout
    Add/Minimum Layout for View Article

--- pages/articles/[articleId].tsx
+++ pages/articles/[articleId].tsx
@@ -1,20 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useArticleData } from '../../lib/article/Article.hooks';
+import ArticleField from '../../components/inputfields/ArticleField';
+import { EditorState, convertFromRaw } from 'draft-js';
+import createImagePlugin from '@draft-js-plugins/image';
 
 interface Props {
   
 }
 
+const imagePlugin = createImagePlugin();
+const plugins = [imagePlugin];
+
 const ViewArticle = (props: Props) => {
+
   const router = useRouter();
   const { articleId } = router.query
-
   const articleData = useArticleData(articleId)
-  console.log(articleData);
+  
+  const [articleState, setArticleState] = useState<EditorState>(
+    EditorState.createEmpty()
+  )
+
+  useEffect(() => {
+    if (articleData === null) return;
+    const contentState = convertFromRaw(JSON.parse(articleData.content));
+    setArticleState(EditorState.createWithContent(contentState));
+  }, [articleData]);
+
+  if (!articleData) return <p>loading</p>
   return (
-    <div className="mt-64">
-      {articleId}
+    <div className="mt-32 mx-2 sm:mx-12 lg:mx-32">
+      <h1 className="text-4xl text-brand font-bold max-w-2xl mx-auto">
+        {articleData.title}
+      </h1>
+      <ArticleField
+        className='mt-8'
+        articleState={articleState}
+        setArticleState={setArticleState}
+        readOnly
+        plugins={plugins}
+      />
     </div>
   )
 }
