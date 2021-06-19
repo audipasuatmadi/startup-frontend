e0e0c33befbab6bd8ce0015cfb99598d54fca947
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-06-19 15:26:52 +0800

    enhance/view all course

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,18 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
-import {
-  RawArticleData,
-  BriefArticleData,
-} from '../lib/article/ArticleActions.types';
-import { useSelector } from 'react-redux';
-import { RootState } from '../states/RootReducer';
-import { isUserDataType } from '../lib/user/userTypes';
+import { PublishedArticleData } from '../lib/article/ArticleActions.types';
 import EditorAPI from '../apis/EditorAPI';
 import { RawDraftContentState } from 'draft-js';
 import ViewArticleCard from '../components/cards/ViewArticleCard';
 export default function Home() {
-  const [articles, setArticles] = useState<RawArticleData[]>([]);
-  const userData = useSelector((state: RootState) => state.userData);
+  const [articles, setArticles] = useState<PublishedArticleData[]>([]);
 
   useEffect(() => {
     const handleGetArticles = async () => {
-      if (!isUserDataType(userData)) return;
-      const resArticles = await EditorAPI.getArticlesByUser(userData.id);
+      const resArticles = await EditorAPI.getAllArticles();
       if (resArticles !== false) {
-        const processedArticles: BriefArticleData[] = resArticles.data.contents.map(
-          ({ content, ...otherArticle }: RawArticleData) => {
+        const processedArticles: PublishedArticleData[] = resArticles.map(
+          ({ content, ...otherArticle }: PublishedArticleData) => {
             const parsedContent: RawDraftContentState = JSON.parse(content);
             const briefDescription = parsedContent.blocks
               .map((block) => (!block.text.trim() && '\n') || block.text)
@@ -35,11 +27,7 @@ export default function Home() {
       }
     };
     handleGetArticles();
-  }, [userData]);
-
-  useEffect(() => {
-    console.log(articles);
-  }, [articles]);
+  }, []);
 
   return (
     <div>
@@ -49,13 +37,14 @@ export default function Home() {
       </Head>
       <section className='mt-20 px-8 flex flex-col gap-5 md:mx-32 lg:mx-64'>
         <header className='font-bold text-xl text-brand'>Articles</header>
-        {articles.map(({ id, title, content, writerId }, key) => (
+        {articles.map(({ id, title, content, createdAt, writer }, key) => (
           <ViewArticleCard
             key={key}
             id={id}
             name={title}
             description={content}
-            writerName={writerId.toString()}
+            writerName={writer.name}
+            createdAt={new Date(createdAt)}
           />
         ))}
       </section>
